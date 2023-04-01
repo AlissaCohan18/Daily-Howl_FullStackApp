e497f21df7456436d1508828532c8e5fc7b2bc32
Alissa Cohan <alissacohan18@gmail.com>
2023-04-01 12:57:41 -0400

    added Nav component and removed pre-installed css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
+import "./App.css";
+import Nav from "./Components/Nav";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-        <p>
-         The Daily Howl
-        </p>
-       
+        <Nav />
+        {/* can also put icon in button  "start" or "end" icon 
+       which places it at beginning or end of button*/}
+        <h1>The Daily Howl</h1>
       </header>
     </div>
   );
