357f74b62abbe081af45f8e9cf0e35ec18bf1efa
Alissa Cohan <alissacohan18@gmail.com>
2023-04-09 16:37:53 -0400

    added jwt for signup and login

--- server/controllers/signing-controller.js
+++ server/controllers/signing-controller.js
@@ -1,18 +1,41 @@
 const { User } = require("../models");
+const jwt = require('jsonwebtoken')
+require("dotenv").config();
 
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
-// signup a user
+// sign Up a user
 const createUser = async (req, res) => {
-    const {email, username, password} = req.body
-  
-    try {
-      const user = await User.signup(email, username, password)
-  
-  
-      res.status(200).json({email, user})
-    } catch (error) {
-      res.status(400).json({error: error.message})
-    }
+  const {email, username, password} = req.body
+
+  try {
+    const user = await User.signup(email, username, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
   }
-  
-  module.exports = { createUser }
+}
+
+// sign In a user
+const loginUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+module.exports = { createUser, loginUser }
