3d67eb94a28457f9bca94ebce438decf99df9e67
Alissa Cohan <alissacohan18@gmail.com>
2023-04-12 15:25:26 -0500

    heroku t/s: updated root package.json

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
 
   return (
     <div>
-      <h1>The Daily Howl</h1>
+      <h1>The Daily Howl trial on 4/12 at 3:24pm</h1>
       <img src={Beagle} className="home-photo" alt="dog" />
     </div>
   );
