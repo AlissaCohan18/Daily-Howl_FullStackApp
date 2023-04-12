564a498c094a96096ad738496695860743a360e9
Alissa Cohan <alissacohan18@gmail.com>
2023-04-11 20:58:57 -0500

    updated path code in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
 app.use(require("./routes"));
 
@@ -25,6 +30,3 @@ mongoose.connect(process.env.MONGODB_URI, {
 mongoose.set("debug", true);
 
 // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
