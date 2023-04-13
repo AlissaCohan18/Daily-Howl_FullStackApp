10649d2d73e03803660362598056eba5d235910e
Alissa Cohan <alissacohan18@gmail.com>
2023-04-13 18:10:34 -0400

    captures meme text and user e-mail for db fetch

--- client/src/component/MemeForm.js
+++ client/src/component/MemeForm.js
@@ -2,27 +2,67 @@ import React from "react";
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Container, Button } from "@mui/material";
-
-
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const MemeForm = () => {
-  const [isMeme, setIsMeme] = useState(false)
+  const { user } = useAuthContext();
+  const [isMeme, setIsMeme] = useState(false);
+  const [memeText, setMemeText] = useState("");
+  const [error, setError] = useState(null);
+  
   const memeAdded = (e) => {
-    console.log(e)
-    if (e !== ""){
+    // console.log(e);
+    if (e !== "") {
       setIsMeme(true);
+      setMemeText(e)
+    } else {
+      setIsMeme(false);
     }
-    else{
-    setIsMeme(false)};
-  }; 
- 
- 
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(memeText)
+    console.log(user.email)
+    const userEmail = user.email
+    const meme = { memeText, userEmail };
+
+    const response = await fetch("/api/memes/:userId", {
+      method: "POST",
+      body: JSON.stringify(meme),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+
+    if (!response.ok) {
+      setError(json.error);
+    }
+    if (response.ok) {
+      setError(null);
+      setMemeText("");
+    }
+  };
+
   return (
     <Container className="memeContainer">
       <div>
-        <TextField onChange={(e)=> memeAdded(e.target.value)} label="meme" sx={{ m: 1, width: "25ch" }} />
+        <TextField
+          onChange={(e) => memeAdded(e.target.value)}
+          label="meme"
+          sx={{ m: 1, width: "25ch" }}
+        />
       </div>
-      <Button className="btn" size="small" disabled={!isMeme} variant="outlined" color="secondary">
+      <Button
+        className="btn"
+        size="small"
+        disabled={!isMeme}
+        variant="outlined"
+        color="secondary"
+        onClick={handleSubmit}
+      >
         Add Meme
       </Button>
     </Container>
