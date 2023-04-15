d906d0731160b213c292fd2cec81ae8ae4da1c57
Alissa Cohan <alissacohan18@gmail.com>
2023-04-15 16:56:05 -0400

    ts heroku

--- server/server.js
+++ server/server.js
@@ -12,18 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....')
-  })
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
-
 
 app.use(require("./routes"));
 
