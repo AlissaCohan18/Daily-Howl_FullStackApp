ed3bd34d6aa87c113527523ac86638f4b5a348a8
Alissa Cohan <alissacohan18@gmail.com>
2023-04-15 16:53:00 -0400

    ts heroku page refresh error: add wildcard to server.js

--- server/server.js
+++ server/server.js
@@ -13,11 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
-  //added for herkou (crashed on refresh)
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
+
 
 
 app.use(require("./routes"));
